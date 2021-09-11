d2a5f310471983537aa6179c448c57c2f10f4270
JoaoDias <joaovictordiasbandini@gmail.com>
2021-09-10 23:39:20 -0300

    Project Created

---
+++ backend/src/test/java/com/joao/dias/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.dias.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
